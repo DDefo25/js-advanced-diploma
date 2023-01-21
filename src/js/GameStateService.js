d8f605e95d3ae84d8058279cb16cc89d9e21bfc7
ddefo25 <ispumamm@gmail.com>
2023-01-21 21:03:37 +0200

    tryings

---
+++ src/js/GameStateService.js
@@ -0,0 +1,17 @@
+export default class GameStateService {
+  constructor(storage) {
+    this.storage = storage;
+  }
+
+  save(state, name = 'state') {
+      this.storage.setItem(name, JSON.stringify(state));
+  }
+
+  load(name = 'state') {
+    try {
+      return JSON.parse(this.storage.getItem(name));
+    } catch (e) {
+      throw new Error('Invalid state');
+    }
+  }
+}
---
+++ src/js/characters/Bowman.js
@@ -0,0 +1,16 @@
+import CharMath from './CharMath';
+
+export default class Bowman extends CharMath {
+  constructor(level, health) {
+    super(level, 25, 25);
+    this.type = 'bowman';
+    this.attackRange = 2;
+    this.moveRange = 2;
+
+    if (health) {
+      this.health = health;
+    }
+  }
+
+  [Symbol.toStringTag] = 'Bowman';
+}
